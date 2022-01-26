fa4a9404c33bb763edca0329dd4c276edde4e025
AOMuiz <abdulwasiumuiz@gmail.com>
2022-01-26 13:24:19 +0100

    finished with homepage design

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,16 @@
+import React, { useState, useEffect } from 'react'
+import { getCategories } from '../services'
 import Link from 'next/link'
-import React from 'react'
-
-const categories = [
-  { name: 'React', slug: 'react' },
-  { name: 'Web Development', slug: 'web-dev' },
-]
 
 const Header = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories)
+    })
+  }, [])
+
   return (
     <div className="container mx-auto mb-8 px-10">
       <div className="inline-block w-full border-b border-blue-400 py-8">
@@ -19,9 +23,12 @@ const Header = () => {
         </div>
         <div className="hidden md:float-left md:contents">
           {categories.map((category) => (
-            <Link key={category.slug} href={`/category/${category.slug}`}>
+            <Link
+              key={category.attributes.Slug}
+              href={`/category/${category.attributes.Slug}`}
+            >
               <span className="mt-2 ml-4 cursor-pointer align-middle font-semibold text-white md:float-right">
-                {category.name}
+                {category.attributes.Name}
               </span>
             </Link>
           ))}
