dd00a5725e0828d69b3019d6692bf66f6b02d3d4
AOMuiz <abdulwasiumuiz@gmail.com>
2022-02-07 18:21:05 +0100

    remove key

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
   return (
     <div className="container mx-auto mb-8 px-5 lg:px-10">
       <div className="inline-block w-full border-b border-blue-400 py-8">
-        <div className="block bg-gradient-to-r from-red-400 to-red-600 bg-clip-text md:float-left">
+        <div className="block bg-gradient-to-r from-red-400 via-blue-500 to-red-600 bg-clip-text md:float-left">
           <Link href="/">
             <span className="cursor-pointer text-3xl font-bold text-transparent text-white lg:text-4xl">
               SalafiBlog
